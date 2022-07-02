49d226bdaa6e9dad8c1a5fcea8e2fffe9824d438
Gunawan Wahyu <greezyzilla@gmail.com>
2022-07-03 00:09:50 +0700

    feat: compile basic core setup for the note app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,77 @@
-export default function App() {
-  return (
-    <div className="text-5xl font-bold bg-gradient-to-bl from-pink-600 via-gray-600 to-indigo-600 w-fit uppercase text-transparent bg-clip-text">Starter app</div>
-  );
+import { Component } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import {
+  ActivePage, ArchivedPage, HomePage, StarredPage,
+} from './components/pages';
+import { NoteInterface } from './interfaces';
+import { getInitialData } from './utils';
+
+interface RootState{
+  notes: NoteInterface[];
+  filter : string;
 }
+
+class App extends Component<any, RootState> {
+  constructor(props : any) {
+    super(props);
+    this.state = {
+      notes: getInitialData(),
+      filter: '',
+    };
+  }
+
+  onArchiveHandle(id : number) {
+    const { notes } = this.state;
+    const newNotes = [...notes];
+    const noteId = newNotes.findIndex((Note : NoteInterface) => +Note.id === +id);
+    newNotes[noteId].archived = !newNotes[noteId].archived;
+    this.setState({ notes: newNotes });
+  }
+
+  onStarHandle(id : number) {
+    const { notes } = this.state;
+    const newNotes = [...notes];
+    const noteId = newNotes.findIndex((Note : NoteInterface) => +Note.id === +id);
+    newNotes[noteId].starred = !newNotes[noteId].starred;
+    this.setState({ notes: newNotes });
+  }
+
+  render() {
+    const { notes, filter } = this.state;
+
+    const components = {
+      all: <HomePage
+        notes={notes}
+        filter={filter}
+        onArchive={(id:number) => this.onArchiveHandle(id)}
+        onStar={(id:number) => this.onStarHandle(id)}
+      />,
+      active: <ActivePage
+        notes={notes}
+        onArchive={(id:number) => this.onArchiveHandle(id)}
+        onStar={(id:number) => this.onStarHandle(id)}
+      />,
+      starred: <StarredPage
+        notes={notes}
+        onArchive={(id:number) => this.onArchiveHandle(id)}
+        onStar={(id:number) => this.onStarHandle(id)}
+      />,
+      archived: <ArchivedPage
+        notes={notes}
+        onArchive={(id:number) => this.onArchiveHandle(id)}
+        onStar={(id:number) => this.onStarHandle(id)}
+      />,
+    };
+
+    return (
+      <Routes>
+        <Route path="/" element={components.all} />
+        <Route path="/active" element={components.active} />
+        <Route path="/starred" element={components.starred} />
+        <Route path="/archived" element={components.archived} />
+      </Routes>
+    );
+  }
+}
+
+export default App;
