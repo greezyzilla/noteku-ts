58897b962a2cb44b7f512ac50396bd450032b03d
Gunawan Wahyu <greezyzilla@gmail.com>
2022-07-31 18:53:35 +0700

    feat: :sparkles: Add Footer

--- src/components/organisms/footer.tsx
+++ src/components/organisms/footer.tsx
@@ -1,5 +1,10 @@
 export default function Footer() {
   return (
-    <div>footer</div>
+    <div className="text-center text-sm text-slate-500">
+      Created with 💖 by&nbsp;
+      <a href="https://github.com/greezyzilla" className="font-semibold hover:text-slate-700">
+        GreezyZilla
+      </a>
+    </div>
   );
 }
