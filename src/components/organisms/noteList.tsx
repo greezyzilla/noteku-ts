53eb2e7dbc5b96d1d08ac7eb720a51990c00fe94
Gunawan Wahyu <greezyzilla@gmail.com>
2022-08-08 02:31:02 +0700

    feat: :sparkles: not found display

--- src/components/organisms/noteList.tsx
+++ src/components/organisms/noteList.tsx
@@ -20,10 +20,25 @@ export default function NoteList(props : NoteListProps) {
   ));
 
   return (
-    <div className="h-full">
-      <div className="grid xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4 overflow-auto h-fit px-5 py-4 box-border w-full">
-        { notesElement }
-      </div>
+    <div className="h-full w-full">
+      {
+        notesElement.length ? (
+          <div className="grid xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4 overflow-auto h-fit px-5 py-4 box-border w-full">
+            { notesElement }
+          </div>
+        ) : (
+          <div className="w-full h-full flex items-center justify-center flex-col">
+            <div className="-mt-64">
+              <img src="./404.gif" alt="" className="w-80 h-80" />
+              <p className="text-center -mt-20 text-slate-600 leading-7">
+                Whooopss... no note found
+                <br />
+                But you can always create the new one!
+              </p>
+            </div>
+          </div>
+        )
+      }
     </div>
   );
 }
